1dcde9612f4cab340b3b6e2d3fc6fb3a509b72a6
Lau Zhan Yeng <lauzhanyeng2001@gmail.com>
2026-05-14 21:37:12 +0800

    create publishvoucher.php

---
+++ publishvoucher.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$user  = $_SESSION['username'] ?? 'Guest';
+///////////////////////cannect database///////////////
+$servername = "localhost";
+$username_db = "root";
+$password_db = "";
+$dbname = "assignment"; 
+
+$conn = mysqli_connect($servername, $username_db, $password_db, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//////////////////////////////////////////
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
