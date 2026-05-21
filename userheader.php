bf938c6c25ff9e0422ee3ac1288006651a985b7b
lian <lianhongzhi9639@gmail.com>
2026-05-22 05:28:53 +0800

    dont care

--- userheader.php
+++ userheader.php
@@ -171,6 +171,7 @@ $current_user = $_SESSION['username'] ?? 'Guest';
         <a href="adminpage.php">🛠️ Dashboard</a>
         <a href="sendalertpage.php">🔔 Send Alert</a>
         <a href="publishvoucher.php">🎟️ Publish Voucher</a>
+        <a href="#settings">⚙️ Settings</a>
         <a href="homepage.php" style="color: #ff4d4d;">🚪Logout</a>
 
     <?php elseif ($role == 'manager'): ?>
